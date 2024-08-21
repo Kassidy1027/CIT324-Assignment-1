993b5903222eb856fbdcddaa9c93b75b4787be47
Kassidy1027 <KNC23@pct.edu>
2024-08-21 18:00:20 -0400

    Progress on Assignment

---
+++ Garden Light Assignment/Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * REFERENCED FROM PREVIOUS SCRIPTS
+ */
+
+
+public class CameraController : MonoBehaviour
+{
+    // Referencing the Player's GameObject
+    public GameObject character;
+
+    // Placeholder for the Distance Between Camera and Player
+    private Vector3 cameraOffset;
+
+    // Start Method, Called Before the First Frame Update
+    void Start()
+    {
+        // Calculating the Offset Between Camera's Position and Player's Position
+        cameraOffset = transform.position - character.transform.position;
+    }
+
+    // LateUpdate Called Once per Frame Once all Updates Functions are Completed
+    void LateUpdate()
+    {
+        // Helping Maintain the Same Offset Between Player and Camera Through the Game
+        transform.position = character.transform.position + cameraOffset;
+    }
+}
---
+++ Garden Light Assignment/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+    // Variable to Hold the Input of Player Movement
+    private float h;
+    private float v;
+
+    // Variable to Hold Speed
+    public int speed;
+
+
+    /*
+     * UNITY DEFAULT METHODS
+     */
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Getting the Value from Player Movement
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        // Moving the Character without Physics
+        transform.Translate(new Vector3 (h, 0, v) * speed * Time.deltaTime);
+
+    }
+}
